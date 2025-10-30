0491c07f6f19d8c106ee1ae3f236a0c1fa0c4a96
linkidtag01-code <linkidtag01@gmail.com>
2025-10-30 11:39:10 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,30 @@
-async headers() {
-  return [
-    {
-      source: '/:sw(sw|sw-1-1-1).js',
-      headers: [
-        { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' },
-        { key: 'Service-Worker-Allowed', value: '/' },
-      ],
-    },
-    {
-      source: '/manifest.webmanifest',
-      headers: [{ key: 'Content-Type', value: 'application/manifest+json' }],
-    },
-  ];
-},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // SW legacy
+        source: '/sw.js',
+        headers: [
+          { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' },
+          { key: 'Service-Worker-Allowed', value: '/' },
+        ],
+      },
+      {
+        // SW nuevo
+        source: '/sw-1-1-1.js',
+        headers: [
+          { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' },
+          { key: 'Service-Worker-Allowed', value: '/' },
+        ],
+      },
+      {
+        // Manifest
+        source: '/manifest.webmanifest',
+        headers: [{ key: 'Content-Type', value: 'application/manifest+json' }],
+      },
+    ];
+  },
+};
+export default nextConfig;
