d5bad8adfa58515924cfaf96e128b2ccfcafe443
linkidtag01-code <linkidtag01@gmail.com>
2025-10-30 11:24:08 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/sw.js',
-        headers: [
-          { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' },
-          { key: 'Service-Worker-Allowed', value: '/' },
-        ],
-      },
-      {
-        source: '/manifest.webmanifest',
-        headers: [{ key: 'Content-Type', value: 'application/manifest+json' }],
-      },
-    ];
-  },
-};
-export default nextConfig;
+async headers() {
+  return [
+    {
+      source: '/:sw(sw|sw-1-1-1).js',
+      headers: [
+        { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' },
+        { key: 'Service-Worker-Allowed', value: '/' },
+      ],
+    },
+    {
+      source: '/manifest.webmanifest',
+      headers: [{ key: 'Content-Type', value: 'application/manifest+json' }],
+    },
+  ];
+},
